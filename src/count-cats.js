8ac2128e77ab57f145f004f8e3e12db223c445bd
kneazle <kneazle@yandex.ru>
2021-10-01 03:08:39 +0300

    feat: add count cats solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function countCats(matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let count = 0;
+
+  matrix.forEach(row => {
+    row.forEach(cell => {
+      if (cell === '^^')
+        count++;
+    })
+  });
+
+  return count;
 }
